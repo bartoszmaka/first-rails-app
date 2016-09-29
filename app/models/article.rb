6e9fb4eb5eada6c5fea7eb60fcac714b5aa3525e
bartoszmaka <moncznybartosz@gmail.com>
2016-09-29 17:51:05 +0200

    deleted old version

--- app/models/article.rb
+++
@@ -1,5 +0,0 @@
-class Article < ApplicationRecord
-  has_many :comments
-  validates :title, presence: true, length: { in: 3..500 }
-  validates :content, presence: true, length: { minimum: 5 }
-end
