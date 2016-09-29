c407adac81dad341dc58f1d354f0a2dcf297db84
bartoszmaka <moncznybartosz@gmail.com>
2016-09-29 17:48:39 +0200

    deleted old version

--- app/models/comment.rb
+++
@@ -1,5 +0,0 @@
-class Comment < ApplicationRecord
-  belongs_to :article
-  validates :author, presence: true, length: { in: 3..200 }
-  validates :content, presence: true, length: { in: 1..15_000 }
-end
