88d8d1c0f7ba8de803b198001f2493692de2c3b2
bartoszmaka <moncznybartosz@gmail.com>
2016-09-30 11:37:28 +0200

    it works lol

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,4 +10,26 @@ class CommentsController < ApplicationController
     @comment.delete
     redirect_to article_path(params[:article_id])
   end
+
+  def new
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.new
+  end
+
+  def create
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.new(comment_params)
+    if @comment.valid?
+      @comment.save
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:author, :content)
+  end
 end
