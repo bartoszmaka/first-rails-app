0b29df95cf59bfb3dd42c07074d1ec764cba99b4
bartoszmaka <moncznybartosz@gmail.com>
2016-11-24 16:20:54 +0100

    added tag validations, changed article index view

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,8 @@
 class Tag < ApplicationRecord
   has_many :article_tags
   has_many :articles, through: :article_tags
+  validates :name, presence: true,
+                   uniqueness: true,
+                   format: { with: /[A-Za-z0-9]+/ },
+                   length: { minimum: 1 }
 end
