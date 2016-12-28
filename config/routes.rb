52c323d116225bae70364aa51b21dde1e81d9ca9
bartoszmaka <moncznybartosz@gmail.com>
2016-12-28 15:04:53 +0100

    add static page for permission denied

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,11 @@ Rails.application.routes.draw do
   end
   resources :tags
   # resources :users
-  get     '/login',   to: 'sessions#new'
-  post    '/login',   to: 'sessions#create'
-  delete  '/logout',  to: 'sessions#destroy', as: 'logout'
-  get     '/users/:id', to: 'users#show', as: 'user'
-  get     '/signup',  to: 'users#new', as: 'signup'
-  post    '/users',   to: 'users#create', as: 'register'
+  get     '/login',         to: 'sessions#new'
+  post    '/login',         to: 'sessions#create'
+  delete  '/logout',        to: 'sessions#destroy',     as: 'logout'
+  get     '/users/:id',     to: 'users#show',           as: 'user'
+  get     '/signup',        to: 'users#new',            as: 'signup'
+  post    '/users',         to: 'users#create',         as: 'register'
+  get     '/denied',        to: 'static#denied'
 end
