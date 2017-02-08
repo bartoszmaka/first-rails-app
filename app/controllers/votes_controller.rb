5124e13b3b7d3a0969d1da019e5f9e459966dcb8
bartoszmaka <moncznybartosz@gmail.com>
2017-02-08 17:28:34 +0100

    implement polymorphic votes

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,10 +1,4 @@
 class VotesController < ApplicationController
-  def new
-    votable = Article.find(params[:article_id])
-    @vote = Vote.new
-    redirect_to votable
-  end
-
   def create
     found_votable = Article.find_by(id: params[:article_id]) if params[:article_id]
     found_votable = Comment.find_by(id: params[:comment_id]) if params[:comment_id]
@@ -19,6 +13,8 @@ class VotesController < ApplicationController
   end
 
   def destroy
+    found_votable = Article.find_by(id: params[:article_id]) if params[:article_id]
+    found_votable = Comment.find_by(id: params[:comment_id]) if params[:comment_id]
     if current_user.voted? found_votable
       Vote.find_by(user: current_user, votable: found_votable).destroy
     end
