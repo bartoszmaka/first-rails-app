fb02d9264dc8cc8f6bb1a093013fb65e5109c90d
bartoszmaka <moncznybartosz@gmail.com>
2017-03-05 17:58:20 +0100

    current user bugfix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < ActionController::Base
 
   def current_user_owns?(whatever)
     false if current_user.nil? || current_user.banned?
-    current_user == whatever.user || current_user.admin?
+    current_user == whatever.user || current_user&.admin?
   end
   helper_method :current_user_owns?
 end
