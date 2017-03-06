c17246ae392f786d45e31a14f3b820728b9378e5
bartoszmaka <moncznybartosz@gmail.com>
2017-03-06 21:50:43 +0100

    views update, add fixed right container

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,6 @@
 class WelcomeController < ApplicationController
   def index
+    @best_scored_articles = Article.order(score: :desc).limit(4)
+    @articles = Article.order(created_at: :desc).limit(25)
   end
 end
