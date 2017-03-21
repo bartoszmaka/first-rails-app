ac27aeaaa925265df7013c9b8bc62a22d2194750
bartoszmaka <moncznybartosz@gmail.com>
2017-03-21 14:06:48 +0100

    refactored detecting banned users

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@ class ArticlesController < ApplicationController
   include ArticlesHelper
   # before_action :authorize, only: [:new, :edit, :update, :destroy, :create]
   before_action :authenticate_user!, except: [:show, :index]
-  before_action :deny_banned_user, only: [:new, :edit, :update, :destroy, :create]
+  before_action :deny_banned_user, except: [:show, :index]
 
   def index
     params[:smart_buttons] = index_buttons
