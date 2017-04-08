63cfd91aefaffc6c8113a1234c0cfe8dd2a27bde
bartoszmaka <moncznybartosz@gmail.com>
2017-04-08 22:28:00 +0200

    fix votes

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,12 +1,22 @@
 class TagsController < ApplicationController
-  def index
-    @q = Tag.ransack(params[:q])
-    @tags = @q.result
-  end
+  expose :q, :foobar
+  expose :tag
+  expose :tags { q.result }
+  expose :articles { q.result }
 
-  def show
-    @tag = Tag.find(params[:id])
-    @q = @tag.articles.ransack(params[:q])
-    @articles = @q.result
+  # def index
+  #   @q = Tag.ransack(params[:q])
+  #   @tags = @q.result
+  # end
+
+  # def show
+  #   @tag = Tag.find(params[:id])
+  #   @q = @tag.articles.ransack(params[:q])
+  #   @articles = @q.result
+  # end
+
+  def foobar
+    return Tag.ransack(params[:q]) if params[:action] == 'index'
+    return Tag.find(params[:id]).articles.ransack(params[:q]) if params[:action] == 'show'
   end
 end
--- app/decorators/article_decorator.rb
+++ app/decorators/article_decorator.rb
@@ -1,4 +1,4 @@
 class ArticleDecorator < Draper::Decorator
   delegate_all
-
+  delegate :class
 end
